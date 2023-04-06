4565424a6241d0c8b9e290718fde6ac45429e109
Marcus R. Brown <contact@marcusrbrown.com>
2023-04-05 23:55:35 -0700

    feat: add TypeScript (#10)

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-function calculateDatesBetweenBeginAndEnd(begin, end) {
+function calculateDatesBetweenBeginAndEnd(begin: string | number | Date, end: string | number | Date) {
   const beginDate = new Date(begin);
   const endDate = new Date(end);
   const daysBetweenBeginAndEnd = Math.ceil((endDate.getTime() - beginDate.getTime()) / (1000 * 60 * 60 * 24));
--- process.ts
+++ process.ts
@@ -1,5 +1,5 @@
 // Find all images without alternate text and give them a red border
-function process() {
+function processImages() {
   var images = document.getElementsByTagName('img');
   for (var i = 0; i < images.length; i++) {
     if (images[i].alt == '') {
