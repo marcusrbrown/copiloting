9629b322a5dc81ccf78a2befe573750df9f79de9
Marcus R. Brown <contact@marcusrbrown.com>
2023-04-07 16:34:07 -0700

    chore: remove old files

--- index.ts
+++
@@ -1,6 +0,0 @@
-function calculateDatesBetweenBeginAndEnd(begin: string | number | Date, end: string | number | Date) {
-  const beginDate = new Date(begin);
-  const endDate = new Date(end);
-  const daysBetweenBeginAndEnd = Math.ceil((endDate.getTime() - beginDate.getTime()) / (1000 * 60 * 60 * 24));
-  return daysBetweenBeginAndEnd;
-}
