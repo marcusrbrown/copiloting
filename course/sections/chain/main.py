dddf855680f73a1b28c0f88c7872061c2c239b13
Marcus R. Brown <contact@marcusrbrown.com>
2023-11-10 16:35:42 -0700

    fix: update `OpenAI` LLM import syntax

--- course/sections/chain/main.py
+++ course/sections/chain/main.py
@@ -1,5 +1,5 @@
 from dotenv import load_dotenv
-from langchain.llms import OpenAI
+from langchain.llms.openai import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain, SequentialChain
 import argparse
